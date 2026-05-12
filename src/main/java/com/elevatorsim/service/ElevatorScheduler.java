507108c2b1a2fca39a7d925b21f06f991d656a29
OMAR <aouichi.omar@etu.uae.ac.ma>
2026-05-12 20:58:57 +0100

    removed unwanted files

--- src/main/java/com/elevatorsim/service/ElevatorScheduler.java
+++
@@ -1,8 +0,0 @@
-package com.elevatorsim.service;
-
-import com.elevatorsim.model.Elevator;
-import java.util.List;
-
-public interface ElevatorScheduler {
-    Elevator selectBestElevator(List<Elevator> elevators, int targetFloor);
-}
